0df5a2ce7395e4d9f31e50e26eb3ad46358b3463
CameronWD <14041@coderacademy.edu.au>
2023-06-19 22:51:55 +1000

    added Profiles class

---
+++ src/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+
+
+# Drivers: driver_id(PK), profile_id(FK), category_id(FK), team_id(FK)
+class Driver(db.Model):
+    driver_id = db.Column(db.Integer, primary_key=True)
+    profile_id = db.Column(db.Integer, db.ForeignKey('profile.profile_id'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.category_id'), nullable=False)
+    team_id = db.Column(db.Integer, db.ForeignKey('team.team_id'), nullabe=False)
+
+# Profiles: profile_id(PK), date_of_birth, first_name, last_name, nationality
+class Profiles(db.Model):
+    profile_id = db.Column(db.Integer, primary_key=True)
+    date_of_birth = db.Column()
+    first_name = db.Column(db.string)
+    last_name = db.Column(db.string)
+    nationality = db.Column(db.string)
+
+# Teams: team_id(PK), name, year_founded, category_id(FK)
+class Teams(db.Model):
+    team_id = db.Column(d)
+
+# Circuits: circuit_id(PK), track_name, location
+
+# Results: results_id(PK), race_id(FK), driver_id(FK), start_position, end_position, points
+
+# Categories: category_id(PK), name, description
+
+# Race: race_id(PK), date, circuit_id(FK), category_id(FK)
+
